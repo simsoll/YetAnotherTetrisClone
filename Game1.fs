bdb5a720ba0441d18062945727776a9c4455e67b
Simon Søllingvraa <simon@soellingvraa.dk>
2014-12-04 18:43:35 +0100

    Added F# + MonoGame template

---
+++ Game1.fs
@@ -0,0 +1,37 @@
+﻿module YetAnotherTetrisClone
+ 
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics
+
+type Game1 () as x =
+    inherit Game()
+ 
+    do x.Content.RootDirectory <- "Content"
+    let graphics = new GraphicsDeviceManager(x)
+    let mutable spriteBatch = Unchecked.defaultof<_>
+    
+    override x.Initialize() =       
+        x.IsMouseVisible <- true
+
+        do base.Initialize()
+        ()
+ 
+    override x.LoadContent() =
+        spriteBatch <- new SpriteBatch(x.GraphicsDevice)
+
+        do base.LoadContent()
+        ()
+ 
+    override x.Update (gameTime) =
+
+        do base.Update (gameTime)
+        ()
+ 
+    override x.Draw (gameTime) =
+        do x.GraphicsDevice.Clear Color.LightGray
+
+        spriteBatch.Begin()
+
+        spriteBatch.End()
+
+        do base.Draw (gameTime)
---
+++ Program.fs
@@ -0,0 +1,7 @@
+﻿open YetAnotherTetrisClone
+ 
+[<EntryPoint>]
+let main argv =
+    use g = new Game1()
+    g.Run()
+    0

458cbb556d03f546a611fee8cf8ff0018f570367
Simon Søllingvraa <simon@soellingvraa.dk>
2014-12-10 22:57:44 +0100

    Added common types folder and file

---
+++ CommonTypes/CommonTypes.fs
@@ -0,0 +1,16 @@
+﻿namespace YetAnotherTetrisClone.CommonTypes
+
+open System
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics
+
+type Tile = 
+    {
+        Position: Vector2
+        Color: Color
+    }
+
+type Tile with
+    static member create x y color = {Position = new Vector2(float32 x, float32 y); Color = color}
+    
+type TileMap = Tile list  
